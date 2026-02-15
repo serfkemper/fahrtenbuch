061643adf0e57fdcab261c634dc5f2f5a4fc0850
Simon Erfkemper <s.erfkemper@web.de>
2026-02-15 01:12:20 +0100

    fix templates favorite route

--- src/app/api/templates/[id]/favorite/route.ts
+++ src/app/api/templates/[id]/favorite/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
-import { prisma } from "@/app/lib/prisma";
+import { prisma } from "../../../lib/prisma";
 
 export async function POST(
   req: NextRequest,
