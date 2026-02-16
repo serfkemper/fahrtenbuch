05ae6d497518b08413be06909aa30e42d454e3f2
Simon Erfkemper <s.erfkemper@web.de>
2026-02-16 18:29:57 +0100

    simplify prisma client init

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log: ["query"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
